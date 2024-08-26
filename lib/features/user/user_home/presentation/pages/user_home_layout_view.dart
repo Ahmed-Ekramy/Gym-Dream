227743e40dad1295bb52c308622bdc959f7c4807
Ahmed Ekramy <132719533+Ahmed-Ekramy@users.noreply.github.com>
2024-08-26 22:00:26 +0300

    custom flexible bar

--- lib/features/user/user_home/presentation/pages/user_home_layout_view.dart
+++ lib/features/user/user_home/presentation/pages/user_home_layout_view.dart
@@ -5,6 +5,7 @@ import 'package:gym_dream/core/app_text_style.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../../../../../core/app_asset.dart';
 import '../../../../../core/app_color.dart';
+import '../widgets/custom_flixibleBar.dart';
 import '../widgets/custom_row_capacity.dart';
 
 class UserHomeLayoutView extends StatelessWidget {
@@ -19,7 +20,7 @@ class UserHomeLayoutView extends StatelessWidget {
             SliverAppBar(
               expandedHeight: 70.h,
               backgroundColor: Colors.transparent, // Set background to transparent
-              flexibleSpace: CustomFlexibleBar(),
+              flexibleSpace: const CustomFlexibleBar(),
               actions: [
                 SvgPicture.asset(
                   color:  AppColor.secondary,
@@ -28,9 +29,7 @@ class UserHomeLayoutView extends StatelessWidget {
                   height: 24.h,
                 ),
               ],
-            )
-
-
+            ),
 
     ],
         ),
@@ -39,60 +38,5 @@ class UserHomeLayoutView extends StatelessWidget {
   }
 }
 
-class CustomFlexibleBar extends StatelessWidget {
-  const CustomFlexibleBar({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return FlexibleSpaceBar(
-      background: Container(
-        decoration: BoxDecoration(
-          color: AppColor.primaryLight,
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(16.r), // Adjust the radius as needed
-            bottomRight: Radius.circular(16.r),
-          ),
-        ),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 16.w), // Add left padding to align
-                  child: CircleAvatar(
-                    radius: 30.r,
-                    backgroundImage: const AssetImage(
-                      AppAsset.boy,
-                    ),
-                  ),
-                ),
-                SizedBox(width: 10.w),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('ID: 123456', style: AppTextStyle.black600S16),
-                    SizedBox(height: 10.h),
-                    Text(
-                      maxLines: 2,
-                      '12 sessions completed, 8 sessions remaining',
-                      style: AppTextStyle.blackOpacity600S14.copyWith(
-                        fontSize: 8.sp,
-                      ),
-                    ),
-                    SizedBox(height: 10.h),
-                    const CustomRowCapacity(),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 
