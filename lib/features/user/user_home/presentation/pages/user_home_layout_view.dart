a59f33bd48c7b496aac47164eb8bf5b7a448b1c4
Ahmed Ekramy <132719533+Ahmed-Ekramy@users.noreply.github.com>
2024-08-26 21:57:42 +0300

    custom row capacity

--- lib/features/user/user_home/presentation/pages/user_home_layout_view.dart
+++ lib/features/user/user_home/presentation/pages/user_home_layout_view.dart
@@ -1,12 +1,11 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gym_dream/core/app_text_style.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
 import '../../../../../core/app_asset.dart';
 import '../../../../../core/app_color.dart';
-import '../widgets/user_home_body_view.dart';
+import '../widgets/custom_row_capacity.dart';
 
 class UserHomeLayoutView extends StatelessWidget {
   const UserHomeLayoutView({super.key});
@@ -20,110 +19,13 @@ class UserHomeLayoutView extends StatelessWidget {
             SliverAppBar(
               expandedHeight: 70.h,
               backgroundColor: Colors.transparent, // Set background to transparent
-              flexibleSpace: FlexibleSpaceBar(
-                background: Container(
-                  decoration: BoxDecoration(
-                    color: AppColor.primaryLight,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(16.r), // Adjust the radius as needed
-                      bottomRight: Radius.circular(16.r),
-                    ),
-                  ),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(left: 16.w), // Add left padding to align
-                            child: CircleAvatar(
-                              radius: 30.r,
-                              backgroundImage: const AssetImage(
-                                AppAsset.boy,
-                              ),
-                            ),
-                          ),
-                          SizedBox(width: 10.w),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('ID: 123456', style: AppTextStyle.black600S16),
-                              SizedBox(height: 10.h),
-                              Text(
-                                maxLines: 2,
-                                '12 sessions completed, 8 sessions remaining',
-                                style: AppTextStyle.blackOpacity600S14.copyWith(
-                                  fontSize: 8.sp,
-                                ),
-                              ),
-                              SizedBox(height: 10.h),
-                              Row(
-                                children: [
-                                  Container(
-                                    decoration: BoxDecoration(
-                                      color: AppColor.green,
-                                      borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(4.r),
-                                        bottomLeft: Radius.circular(4.r),
-                                      ),
-                                    ),
-                                    width: 64.w,
-                                    height: 14.h,
-                                    child: Center(
-                                      child: Text(
-                                        'Active',
-                                        style: AppTextStyle.white500S7,
-                                      ),
-                                    ),
-                                  ),
-                                  Container(
-                                    decoration: BoxDecoration(
-                                      color: AppColor.yellowLight,
-                                    ),
-                                    width: 64.w,
-                                    height: 14.h,
-                                    child: Center(
-                                      child: Text(
-                                        'Active',
-                                        style: AppTextStyle.blackOpacity500S7,
-                                      ),
-                                    ),
-                                  ),
-                                  Container(
-                                    decoration: BoxDecoration(
-                                      color: AppColor.redLight,
-                                      borderRadius: BorderRadius.only(
-                                        topRight: Radius.circular(4.r),
-                                        bottomRight: Radius.circular(4.r),
-                                      ),
-                                    ),
-                                    width: 64.w,
-                                    height: 14.h,
-                                    child: Center(
-                                      child: Text(
-                                        'Active',
-                                        style: AppTextStyle.blackOpacity500S7,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              flexibleSpace: CustomFlexibleBar(),
               actions: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    FontAwesomeIcons.bell,
-                    color: AppColor.secondary,
-                    size: 24.sp,
-                  ),
+                SvgPicture.asset(
+                  color:  AppColor.secondary,
+                  AppAsset.bell,
+                  width: 24.w,
+                  height: 24.h,
                 ),
               ],
             )
@@ -136,3 +38,61 @@ class UserHomeLayoutView extends StatelessWidget {
     );
   }
 }
+
+class CustomFlexibleBar extends StatelessWidget {
+  const CustomFlexibleBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FlexibleSpaceBar(
+      background: Container(
+        decoration: BoxDecoration(
+          color: AppColor.primaryLight,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(16.r), // Adjust the radius as needed
+            bottomRight: Radius.circular(16.r),
+          ),
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 16.w), // Add left padding to align
+                  child: CircleAvatar(
+                    radius: 30.r,
+                    backgroundImage: const AssetImage(
+                      AppAsset.boy,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10.w),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('ID: 123456', style: AppTextStyle.black600S16),
+                    SizedBox(height: 10.h),
+                    Text(
+                      maxLines: 2,
+                      '12 sessions completed, 8 sessions remaining',
+                      style: AppTextStyle.blackOpacity600S14.copyWith(
+                        fontSize: 8.sp,
+                      ),
+                    ),
+                    SizedBox(height: 10.h),
+                    const CustomRowCapacity(),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
