b2c087f9d5fac1d9932b6062eb22c2eb9ce337b7
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-08-28 15:51:49 +0300

    1- add admin layout feature (Botton nav bar) 2-  add navigation class

--- lib/features/user/user_home/presentation/pages/user_home_layout_view.dart
+++ lib/features/user/user_home/presentation/pages/user_home_layout_view.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:gym_dream/core/app_text_style.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:qr_flutter/qr_flutter.dart';
-import '../../../../../common/widgets/custom_button_widget.dart';
+
 import '../../../../../core/app_asset.dart';
 import '../../../../../core/app_color.dart';
 import '../manager/home_user_cubit.dart';
 import '../manager/home_user_state.dart';
-import '../widgets/custom_flixibleBar.dart';
 
 class UserHomeLayoutView extends StatelessWidget {
   const UserHomeLayoutView({super.key});
