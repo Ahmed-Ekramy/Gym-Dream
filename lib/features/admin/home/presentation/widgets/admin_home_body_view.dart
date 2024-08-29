3c86f42af936d7771ef66c141ff6162c610e8cc7
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-08-29 17:04:44 +0300

    1- make custom warning massage in home admin

--- lib/features/admin/home/presentation/widgets/admin_home_body_view.dart
+++ lib/features/admin/home/presentation/widgets/admin_home_body_view.dart
@@ -58,56 +58,7 @@ class _AdminHomeBodyViewState extends State<AdminHomeBodyView> {
               ],
             ),
           ),
-        ),
-        const CustomWarningMassage(),
-        SizedBox(height: 16.h),
-        Padding(
-          padding: EdgeInsetsDirectional.symmetric(horizontal: 16.w),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Recently scanned',
-                style: AppTextStyle.blackOpacity400S10,
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  SizedBox(
-                    width: 160.w,
-                    height: 50,
-                    child: Stack(
-                      clipBehavior: Clip.none,
-                      children: [
-                        for (int i = 0; i < imageList.take(5).length; i++)
-                          Positioned(
-                            left: i * 40.0,
-                            child: CircleAvatar(
-                              backgroundImage: AssetImage(imageList[i]),
-                              radius: 20.r,
-                            ),
-                          ),
-                      ],
-                    ),
-                  ),
-                  if (imageList.length > 5)
-                    CircleAvatar(
-                      radius: 20.r,
-                      backgroundColor: Colors.orange.shade50,
-                      child: Text(
-                        '+${imageList.length - 5}',
-                        style: TextStyle(
-                          color: Colors.orange,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16.sp,
-                        ),
-                      ),
-                    ),
-                ],
-              )
-            ],
-          ),
-        ),
+        )
       ],
     );
   }
