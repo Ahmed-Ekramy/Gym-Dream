74f03a50a76df8386023a40a802f5ccb952e8205
Ahmed Ekramy <132719533+Ahmed-Ekramy@users.noreply.github.com>
2024-08-29 16:51:59 +0300

    edite home screen edite nav bar finish exercises screen ui

--- lib/features/user/user_home/presentation/pages/user_home_layout_view.dart
+++ lib/features/user/user_home/presentation/pages/user_home_layout_view.dart
@@ -8,7 +8,6 @@ import '../../../../../core/app_color.dart';
 import '../../../../../core/app_text_style.dart';
 import '../manager/home_user_cubit.dart';
 import '../manager/home_user_state.dart';
-import '../widgets/bottomNav.dart';
 import '../widgets/custom_flixibleBar.dart';
 import '../widgets/custom_row_capacity.dart';
 
@@ -20,6 +19,7 @@ class UserHomeLayoutView extends StatelessWidget {
     return BlocBuilder<HomeUserCubit, HomeUserState>(
       builder: (context, state) {
         final cubit = HomeUserCubit.get(context);
+
         return SafeArea(
           child: Scaffold(
             body: NotificationListener<ScrollNotification>(
@@ -93,7 +93,7 @@ class UserHomeLayoutView extends StatelessWidget {
                 ],
               ),
             ),
-            bottomNavigationBar: const BottomNavBarUser(),
+            bottomNavigationBar: _BottomNavBar(),
           ),
         );
       },
@@ -101,4 +101,68 @@ class UserHomeLayoutView extends StatelessWidget {
   }
 }
 
+class _BottomNavBar extends StatefulWidget {
+  @override
+  __BottomNavBarState createState() => __BottomNavBarState();
+}
 
+class __BottomNavBarState extends State<_BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    final cubit = HomeUserCubit.get(context);
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height: cubit.isBottomNavVisible ? 40.h : 0.0,
+      child: Wrap(
+        children: [
+          BottomNavigationBar(
+            backgroundColor: AppColor.orangeLight,
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: AppColor.primary,
+            unselectedItemColor: AppColor.grey,
+            selectedLabelStyle: AppTextStyle.orange700S16.copyWith(fontSize: 10.sp),
+            onTap: (value) {
+              cubit.changeIndex(value);
+            },
+            currentIndex: cubit.currentIndex,
+            items: [
+              BottomNavigationBarItem(
+                  icon: SvgPicture.asset(
+                    AppAsset.home,
+                    width: 24.w,
+                    height: 24.h,
+                    colorFilter: ColorFilter.mode(
+                      cubit.currentIndex == 0 ? AppColor.primary : AppColor.grey,
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                  label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: SvgPicture.asset(
+                    AppAsset.history,
+                    width: 24.w,
+                    height: 24.h,
+                    colorFilter: ColorFilter.mode(
+                      cubit.currentIndex == 1 ? AppColor.primary : AppColor.grey,
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                  label: 'History'),
+              BottomNavigationBarItem(
+                  icon: SvgPicture.asset(
+                    AppAsset.exercises,
+                    width: 24.w,
+                    height: 24.h,
+                    colorFilter: ColorFilter.mode(
+                      cubit.currentIndex == 2 ? AppColor.primary : AppColor.grey,
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                  label: 'Exercises'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
