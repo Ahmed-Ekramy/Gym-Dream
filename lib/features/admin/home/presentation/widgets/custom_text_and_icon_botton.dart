0c9ad578b628fe46e59c3a4ed36cd8bc7b9eaa58
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-08-31 01:11:46 +0300

    refactor admin home ui

---
+++ lib/features/admin/home/presentation/widgets/custom_text_and_icon_botton.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gym_dream/core/app_color.dart';
+import 'package:gym_dream/core/app_text_style.dart';
+
+class BottomTextAndIconButton extends StatelessWidget {
+  const BottomTextAndIconButton(
+      {super.key,
+      required this.title,
+      this.icon,
+      this.textStyle,
+      this.backGroundcolor,
+      this.heigh,
+      this.width});
+  final String title;
+  final Widget? icon;
+  final TextStyle? textStyle;
+  final double? heigh;
+  final double? width;
+  final Color? backGroundcolor;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+          height: heigh ?? 40.h,
+          width: width ?? 80.h,
+          decoration: BoxDecoration(
+            color: backGroundcolor ?? AppColor.primary,
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              icon ?? const SizedBox(),
+              SizedBox(width: 5.w),
+              Text(
+                title,
+                style: textStyle ?? AppTextStyle.white700S14,
+              ),
+            ],
+          )),
+    );
+  }
+}
