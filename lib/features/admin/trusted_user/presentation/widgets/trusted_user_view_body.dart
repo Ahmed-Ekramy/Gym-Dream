42dae12c25da78e427100503216785670ef49bf4
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-08-31 23:46:07 +0300

    1- add the done botton

--- lib/features/admin/trusted_user/presentation/widgets/trusted_user_view_body.dart
+++ lib/features/admin/trusted_user/presentation/widgets/trusted_user_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gym_dream/common/widgets/custom_button_widget.dart';
 import 'package:gym_dream/core/app_asset.dart';
 import 'package:gym_dream/features/admin/trusted_user/presentation/widgets/days_left_widget.dart';
 import 'package:gym_dream/features/admin/trusted_user/presentation/widgets/personal_information.dart';
@@ -11,25 +12,34 @@ class TrustedUserViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.w),
-        child: Column(
-          children: [
-            SizedBox(height: 16.h),
-            const TrustedOrNotTrustedUserHeader(
-              icon: AppAsset.check,
-              title: 'Trusted User',
-            ),
-            SizedBox(height: 8.h),
-            const UserImage(
-              image: AppAsset.boy,
-            ),
-            SizedBox(height: 16.h),
-            const PersonalInformation(),
-            SizedBox(height: 16.h),
-            const DaysLeftWidget()
-          ],
+    return SingleChildScrollView(
+      child: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          child: Column(
+            children: [
+              SizedBox(height: 16.h),
+              const TrustedOrNotTrustedUserHeader(
+                icon: AppAsset.check,
+                title: 'Trusted User',
+              ),
+              SizedBox(height: 8.h),
+              const UserImage(
+                image: AppAsset.boy,
+              ),
+              SizedBox(height: 16.h),
+              const PersonalInformation(),
+              SizedBox(height: 16.h),
+              const DaysLeftWidget(),
+              SizedBox(height: 32.h),
+              CustomButton(
+                height: 35.h,
+                width: double.infinity,
+                title: 'Done',
+                onPressed: () {},
+              )
+            ],
+          ),
         ),
       ),
     );
