a7a907c8a15c5db9f638905cf647403275ab29bd
don-okal <ahmedokal6996@gmail.com>
2024-09-01 16:12:38 +0300

    validate create password for admin

--- lib/features/admin/home/presentation/widgets/admin_recently_scanned.dart
+++ lib/features/admin/home/presentation/widgets/admin_recently_scanned.dart
@@ -8,7 +8,7 @@ import 'package:gym_dream/core/app_asset.dart';
 import 'package:gym_dream/core/app_text_style.dart';
 import 'package:gym_dream/features/admin/home/presentation/widgets/admin_users_images.dart';
 import 'package:gym_dream/features/admin/home/presentation/widgets/custom_text_and_icon_botton.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+// import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class AdminRecentlyScanned extends StatelessWidget {
   const AdminRecentlyScanned({
@@ -50,21 +50,22 @@ class AdminRecentlyScanned extends StatelessWidget {
 Future<void> scanNow(BuildContext context) async {
   String scanResult;
   try {
-    scanResult = await FlutterBarcodeScanner.scanBarcode(
-      '#ff6666',
-      'Cancel',
-      true,
-      ScanMode.QR,
-    );
+    // scanResult = await FlutterBarcodeScanner.scanBarcode(
+    //   '#ff6666',
+    //   'Cancel',
+    //   true,
+    //   ScanMode.QR,
+    // );
   } on PlatformException {
     scanResult = 'Failed to get platform version.';
   }
 
-  if (scanResult.isNotEmpty && scanResult != '-1') {
-    debugPrint(scanResult);
-    bool isValid = isValidPerson(scanResult);
+  // if (scanResult.isNotEmpty && scanResult != '-1')
+  if (true) {
+    // debugPrint(scanResult);
+    // bool isValid = isValidPerson(scanResult);
 
-    if (isValid) {
+    if (false) {
       // If  valid,
       // AppNavigation.navigateTo(
       //   context: context,
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gym_dream/bloc_observer.dart.dart';
 import 'package:gym_dream/service_locator.dart';
 import 'package:gym_dream/user_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await ScreenUtil.ensureScreenSize();
   ServiceLocator().setUpServiceLocator();
   Bloc.observer = MyBlocObserver();
 
