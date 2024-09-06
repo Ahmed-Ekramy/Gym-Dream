5223a36dcced5929cbb8cf1db9a4fc81d2615e9b
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-09-07 00:40:09 +0300

    1- add the base of the history view

---
+++ lib/features/admin/history/presentation/widgets/admin_history_view_body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AdminHistoryViewBody extends StatelessWidget {
+  const AdminHistoryViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Column(
+        children: [
+          Text('HistoryViewBody'),
+        ],
+      ),
+    );
+  }
+}
