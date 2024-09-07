5a9f9516a771ceb9f8a72b6a479a17ccc6e08ea3
Ahmed Ekramy <132719533+Ahmed-Ekramy@users.noreply.github.com>
2024-09-07 14:46:50 +0300

    history recenr scanner

--- lib/features/admin/history/presentation/widgets/admin_history_view_body.dart
+++ lib/features/admin/history/presentation/widgets/admin_history_view_body.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gym_dream/features/admin/history/presentation/widgets/recent_scanner_date.dart';
+import 'custom_appBar_history.dart';
+import 'custom_container_schedule_admin.dart';
+import 'custom_text_rich.dart';
 
 class AdminHistoryViewBody extends StatelessWidget {
   const AdminHistoryViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Column(
-        children: [
-          Text('Admin History View Body'),
-        ],
-      ),
+    return SafeArea(
+      child: CustomScrollView(slivers: [
+        const CustomAppBarHistory(),
+        SliverToBoxAdapter(child: SizedBox(height: 8.h)),
+        const CustomTextRich(),
+        SliverToBoxAdapter(child: SizedBox(height: 8.h)),
+        const CustomContainerScheduleAdmin(),
+        const RecentScannerDate(),
+      ]),
     );
   }
 }
+
+
+
+
+
+
+
